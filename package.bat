aa5bbe2743371e2937569ea450c587f818ef210b
Clement Levallois <clementlevallois@protonmail.com>
2021-01-11 23:23:59 +0100

    update action

--- package.bat
+++ package.bat
@@ -1,14 +1,13 @@
-jlink --no-header-files --no-man-pages --module-path mods/core --add-modules net.clementlevallois.nocodeapp.core --bind-services --output image
-PAUSE
-REM image/bin/java --module-path mods -m net.clementlevallois.nocodeapp.core/net.clementlevallois.nocodeappcore.controller.RealMain
+jlink --no-header-files --no-man-pages --module-path win/core --add-modules net.clementlevallois.nocodeapp.core --bind-services --output image --compress=1
+
+REM image/bin/java --module-path win -m net.clementlevallois.nocodeapp.core/net.clementlevallois.nocodeappcore.controller.RealMain
 
 REM WINDOWS
-jpackage --type msi --win-menu --win-menu-group "No-code" --win-dir-chooser --win-shortcut --win-dir-chooser --win-console -n "No-code App Packaged" --module-path mods/core -m net.clementlevallois.nocodeapp.core/net.clementlevallois.nocodeappcore.controller.RealMain
+jpackage --type msi --win-menu --win-menu-group "No-code" --win-dir-chooser --win-shortcut --win-dir-chooser --win-console -n "nocode-app" --module-path win/core --runtime-image image -m net.clementlevallois.nocodeapp.core/net.clementlevallois.nocodeappcore.controller.RealMain
 PAUSE
 
 REM MACOS
-jpackage --type dmg -n "no code app" --module-path mods/core --runtime-image image -m net.clementlevallois.nocodeapp.core/net.clementlevallois.nocodeappcore.controller.RealMain
-PAUSE
+REM jpackage --type dmg -n "no code app" --module-path mods/core --runtime-image image -m net.clementlevallois.nocodeapp.core/net.clementlevallois.nocodeappcore.controller.RealMain
 
 
 REM attention ne pas mettre d'options après le main module à la fin de jpackage
